cb025b903c91225f7bce63f24c7cf5ba0bf2129a
camillagianchini <gianchinicamilla@gmail.com>
2025-05-26 16:55:59 +0200

    merged

--- app/src/main/java/it/progmob/myconcerts/WelcomeActivity.kt
+++ app/src/main/java/it/progmob/myconcerts/WelcomeActivity.kt
@@ -8,7 +8,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.MainActivity
+//import it.progmob.myconcerts.MainActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
